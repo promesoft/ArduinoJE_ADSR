2fffc580e54050f1b10be8e25cb907174c6eb964
promesoft <github@shard.dk>
2019-04-07 19:30:48 +0200

    Update ArduinoJE_ADSR1.ino

--- ArduinoJE_ADSR1/ArduinoJE_ADSR1.ino
+++ ArduinoJE_ADSR1/ArduinoJE_ADSR1.ino
@@ -16,7 +16,27 @@ void setupDataStruct(){
     digitalWrite(LEDData[i][0], HIGH);// Turn on LED pins one by one
     delay(300);
   }
+  pinMode(Gate, INPUT);               // Set Gate as input
+  pinMode(Trig, INPUT);               // Set Trig as input
+  pinMode(PWMOut, OUTPUT);            // Set PWM as Output
 } 
+
+void setupAnaloguePins(){
+  pinMode(RV1, INPUT);                // Set Potentiometers as input
+  pinMode(RV2, INPUT);                // Set Potentiometers as input
+  pinMode(RV3, INPUT);                // Set Potentiometers as input
+  pinMode(RV4, INPUT);                // Set Potentiometers as input
+  pinMode(RV5, INPUT);                // Set Potentiometers as input
+  pinMode(CV, INPUT);                 // Set CV as input
+  pinMode(SW2_1, INPUT_PULLUP);              // Set SW2_1 as input
+  pinMode(SW2_2, INPUT_PULLUP);              // Set SW2_2 as input
+/*  pinMode(SW2_1, INPUT);              // Set SW2_1 as input
+  pinMode(SW2_2, INPUT);              // Set SW2_2 as input
+    // Enable internal pull-up resistors  
+  digitalWrite(SW2_1, HIGH);
+  digitalWrite(SW2_2, HIGH);*/
+}
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(38400);
@@ -27,23 +47,37 @@ void setup() {
   Serial.println(F(__DATE__));
   delay(50);  
   setupDataStruct();
-  delay(5000);  
+  delay(2000);  
 }
 
 // the loop function runs over and over again forever
 void loop() {
   updateLED();
   updatePWM();
-
+  readPots();
+  readSwitch();
 }
 
 /* =====================================================
-==============Update LED's based on LED Values==========
+==============Send analogie PWM Value===================
 ======================================================*/ 
 void updatePWM(){
-  for (int i=0; i <= 3; i++){
-    digitalWrite(LEDData[i][0], LEDData[i][1] == 1);// 
-  }
+  
+}
+/* =====================================================
+==============Read Potentiometer Values=================
+======================================================*/ 
+void readPots(){
+  
+}
+/* =====================================================
+==============Read Switch Values========================
+======================================================*/ 
+void readSwitch(){
+  if (analogRead(SW2_1) = 0) LEDData[2][1] = 0; 
+  if (analogRead(SW2_2) = 0) LEDData[3][1] = 0; 
+  if (analogRead(SW2_1) > 0) LEDData[2][1] = 1; 
+  if (analogRead(SW2_2) > 0) LEDData[3][1] = 1; 
 }
 /* =====================================================
 ==============Update LED's based on LED Values==========
@@ -53,8 +87,8 @@ void updateLED(){
     digitalWrite(LEDData[i][0], LEDData[i][1] == 1);// 
   }
 
-/*  delay(1);
-  if ( millis() < lastbuttonupdate + 10000 ){
+  delay(1);
+/*  if ( millis() < lastbuttonupdate + 10000 ){
   */
 }
 
