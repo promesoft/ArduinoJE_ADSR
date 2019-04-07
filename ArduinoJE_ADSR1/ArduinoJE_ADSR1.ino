b70cf9f6ddc1f4a6b8ca8ff916911dfbe72417de
promesoft <github@shard.dk>
2019-04-07 15:19:12 +0200

    buildup

--- ArduinoJE_ADSR1/ArduinoJE_ADSR1.ino
+++ ArduinoJE_ADSR1/ArduinoJE_ADSR1.ino
@@ -1,37 +1,28 @@
 /*
-  Blink
 
-  Turns an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
 */
+#include "global.h";
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(7, OUTPUT);
+  // initialize digital pin LED1-4 as an output.
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(7, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED1, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(7, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED2, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED3, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED4, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED1, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+  digitalWrite(LED2, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED3, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED4, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
---
+++ ArduinoJE_ADSR1/global.h
@@ -0,0 +1,11 @@
+#include <EEPROM.h>
+/* -----------------------------------------------
+ *  GLOBAL DEFINES
+ * -----------------------------------------------
+ */
+
+//OVERRIDE PINS FOR ADSR
+#define LED1 7 // Back next to pwr
+#define LED2 2 // Front
+#define LED3 3 // Front
+#define LED4 4 // Front
